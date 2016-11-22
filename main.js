a1c272b79e9fa2f405452de242af02110a8b3fd3
Jazzyst <andriy.zhuravlov@gmail.com>
2016-11-22 21:56:05 +0200

    21.55

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var currentPos = 0;
 function animate() {  
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');  
+
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
-  ctx.fillRect(320, currentPos, 20, 20);
+  
   currentPos +=1;
-  // currentPos += Math.random() * (4 - 1) + 1;
   ctx.fillStyle = getRndColor(); 
 
   // ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
@@ -16,7 +16,10 @@ function animate() {
   }
   requestAnimationFrame(animate);
   
-
+  for(var i = 0; i < arr.length; i++){
+    y++;
+    ctx.fillRect(rect.x, rect.y, 20, 20);
+  }
 }
 
 document.body.onload = animate;
@@ -27,3 +30,14 @@ function getRndColor() {
         b = 255*Math.random()|0;
     return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
+
+function Rect(x, y, color, speed){
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.speed = speed;
+}
+
+var rect = new Rect(x, y, color, speed);
+var arr = [];
+arr.push(rect);
