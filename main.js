3d0d093410e94428321528cf3d4c9d031a1ad2db
Jazzyst <andriy.zhuravlov@gmail.com>
2016-11-23 23:37:48 +0200

    23.37

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function animate() {
   var ctx = canvas.getContext('2d');  
 
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
-  ctx.fillRect(320, currentPos, 20, 20);
+ 
   currentPos += 1;
   if(currentPos >= canvas.clientHeight) {
     currentPos = 0;
